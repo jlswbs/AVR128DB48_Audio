4246bbc146129d625bab49af50d51b941a8605a0
jlswbs <jlswbs@users.noreply.github.com>
2026-07-11 22:15:56 +0200

    Add files via upload

---
+++ DDS_Sine/DDS_Sine.ino
@@ -0,0 +1,52 @@
+// DDS sine generator //
+
+#define SAMPLE_RATE 44100 
+
+uint16_t sineTable[256];
+
+volatile uint32_t phaseAccumulator = 0;
+volatile uint32_t phaseIncrement = 0;
+
+void setFrequency(float freq) { phaseIncrement = (uint32_t)(((uint64_t)freq << 32) / SAMPLE_RATE); }
+
+void setup() {
+
+  for (int i = 0; i < 256; i++) {
+
+    float angle = (2.0 * M_PI * i) / 256.0;
+    sineTable[i] = (uint16_t)((sin(angle) + 1.0) * 511.5);
+
+  }
+
+  PORTD.PIN6CTRL = PORT_ISC_INPUT_DISABLE_gc;
+  //VREF.DAC0REF = VREF_REFSEL_VDD_gc | VREF_ALWAYSON_bm;
+  VREF.DAC0REF = VREF_REFSEL_2V048_gc | VREF_ALWAYSON_bm;
+  DAC0.CTRLA = DAC_ENABLE_bm | DAC_OUTEN_bm | DAC_RUNSTDBY_bm;
+
+  setFrequency(220.0f);
+
+  TCB0.CTRLA = 0; 
+  TCB0.CTRLB = TCB_CNTMODE_INT_gc; 
+  TCB0.CCMP = (F_CPU / SAMPLE_RATE) - 1; 
+  TCB0.INTCTRL = TCB_CAPT_bm; 
+  TCB0.CTRLA = TCB_ENABLE_bm | TCB_CLKSEL_CLKDIV1_gc;
+
+}
+
+void loop() {
+
+}
+
+ISR(TCB0_INT_vect) {
+
+  phaseAccumulator += phaseIncrement;
+
+  uint8_t tableIndex = phaseAccumulator >> 24;
+  uint16_t val = sineTable[tableIndex];
+
+  DAC0.DATAL = (val & 0x03) << 6;
+  DAC0.DATAH = val >> 2;
+
+  TCB0.INTFLAGS = TCB_CAPT_bm;
+
+}
